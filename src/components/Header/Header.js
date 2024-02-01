b1870d0786734146aed5216e690d0b28e27c2d40
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-02-01 23:42:33 +0530

    update header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,19 @@ export default function Header() {
   return (
     <header className="header">
       <Link to="/" className="header-logo">
-        <img className="header-logo__img" src={logo} alt="To-do-app Logo" />
+        <div className="header-logo__wrapper">
+          <img
+            className="header-logo__wrapper-img"
+            src={logo}
+            alt="To-do-app Logo"
+          />
+          <p className="header-logo__wrapper-text">TO DO IN TOronto</p>
+        </div>
       </Link>
-      <nav>
-        <ul>
-          <li>Events </li>
-          <li>Calendar</li>
+      <nav className="header-nav">
+        <ul className="header-nav__list">
+          <li className="header-nav__list-item">Events </li>
+          <li className="header-nav__list-item">Calendar</li>
         </ul>
       </nav>
     </header>
