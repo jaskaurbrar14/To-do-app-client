cbb6429910ee24929cad8325aec9e793a8b44853
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-02-02 00:02:54 +0530

    design touch ups

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,22 +3,19 @@ import city from "../../Assets/Images/city.png";
 export default function Main() {
   return (
     <main className="main">
-      <p>
+      <h1 className="main__text">
         Welcome to the city are you looking forward to the exciting things
-        happening around you{" "}
-      </p>
-      <button>Click here to join the excitement</button>
-      <section>
-        <div>
-          <img className="header-logo__img" src={city} alt="To-do-app Logo" />
-        </div>
-        <div>
-          {" "}
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
-        </div>
+        happening around you!
+      </h1>
+      <button className="main__button" type="submit">
+        Click here to join the excitement
+      </button>
+      <section className="main__section">
+        <img className="main__section-img" src={city} alt="To-do-app Logo" />
+        <p className="main__section-text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
       </section>
     </main>
   );
